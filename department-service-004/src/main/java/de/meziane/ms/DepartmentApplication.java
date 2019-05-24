91820feb70df7b4209e23e991118b1af48a3aa42
meziano <me.kettou@gmail.com>
2019-05-24 13:36:55 +0200

    Commit after first step.

---
+++ department-service-004/src/main/java/de/meziane/ms/DepartmentApplication.java
@@ -0,0 +1,16 @@
+package de.meziane.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author M. Kettou
+ */
+
+@SpringBootApplication
+public class DepartmentApplication 
+{
+    public static void main( String[] args ) {
+    	SpringApplication.run(DepartmentApplication.class, args);        
+    }
+}
---
+++ employee-service-004/src/main/java/de/meziane/ms/EmployeeApplication.java
@@ -0,0 +1,15 @@
+package de.meziane.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author M. Kettou
+ */
+@SpringBootApplication
+public class EmployeeApplication 
+{
+    public static void main( String[] args ) {
+    	SpringApplication.run(EmployeeApplication.class, args);
+    }
+}
